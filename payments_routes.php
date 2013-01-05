7a9b6565630ac0aeb5171966f3fb19f8268ee49e
Eivar Montenegro <e.mont01@gmail.com>
2013-01-04 22:11:34 -0500

    adding new methods to IInvoice to manage the invoice status: outstading, overdue and paid

--- payments_routes.php
+++ payments_routes.php
@@ -3,16 +3,19 @@ use Laravel\Routing\Route;
 
 /**
  * laravel payments routes
+ * You must define the logged_in filter
  */
-//list available payment gateways
-Route::get('(:bundle)/(:any)', 'payments::payments@chooseMethod');
-//process payment response
-Route::any('(:bundle)/process/(:any)', 'payments::payments@processPayment');
+Route::group(array('before' => 'logged_in'), function()
+{
+	//list available payment gateways
+	Route::get('(:bundle)/(:any)', 'payments::payments@chooseMethod');
+	//process payment response
+	Route::any('(:bundle)/process/(:any)', 'payments::payments@processPayment');
 
-//dummy payment testing (if you do not have a valid test account)
-Route::get('(:bundle)/test/paguelofacil', array(
-	'as' => 'payments_test_paguelo_facil',
-	'uses' => 'payments::test@pagueloFacil',
-));
+	//dummy payment testing (if you do not have a valid test account)
+	Route::get('(:bundle)/test/paguelofacil', array(
+		'as' => 'payments_test_paguelo_facil',
+		'uses' => 'payments::test@pagueloFacil',
+	));
 
-
+});
